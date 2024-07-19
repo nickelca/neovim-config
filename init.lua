ac86e09c6bce0ea4e78cf3bf70949e99dc0b99e1
Chase Nickel <nickel5@outlook.com>
2024-07-18 21:05:52 -0500

    Undotree, fugitive, harpoon

--- init.lua
+++ init.lua
@@ -130,6 +130,41 @@ require('lazy').setup({
     },
 
     {
+        'theprimeagen/harpoon',
+        config = function()
+            local mark = require 'harpoon.mark'
+            local ui = require 'harpoon.ui'
+
+            vim.keymap.set('n', '<leader>a', mark.add_file)
+            vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+
+            vim.keymap.set('n', '<C-h>', function()
+                ui.nav_file(1)
+            end)
+            vim.keymap.set('n', '<C-t>', function()
+                ui.nav_file(2)
+            end)
+            vim.keymap.set('n', '<C-n>', function()
+                ui.nav_file(3)
+            end)
+            vim.keymap.set('n', '<C-s>', function()
+                ui.nav_file(4)
+            end)
+        end,
+    },
+
+    {
+        'mbbill/undotree',
+    },
+
+    {
+        'tpope/vim-fugitive',
+        config = function()
+            vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+        end,
+    },
+
+    {
         'nvim-telescope/telescope.nvim',
         event = 'VimEnter',
         branch = '0.1.x',
