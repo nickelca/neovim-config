a56585b7cb3f799e62db2cabe9bb667fbf69daae
Chase Nickel <nickel5@outlook.com>
2024-07-19 18:45:31 -0500

    Keybind for git push

--- lua/plugin/fugitive.lua
+++ lua/plugin/fugitive.lua
@@ -1,1 +1,6 @@
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+vim.keymap.set('n', '<leader>gs', function()
+    vim.cmd [[Git]]
+end)
+vim.keymap.set('n', '<leader>gp', function()
+    vim.cmd [[Git push]]
+end)
